06d1110cdad252a9a991aba4d6f26d241a8b8e7c
dev-jv <dev.javierleon@gmail.com>
2021-01-20 23:34:46 -0500

    three

--- server.js
+++ server.js
@@ -2,17 +2,19 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    // res.send('Although')
-    let salida = {
-        nombre: 'fernando',
-        edad:32,
-        url: req.url
-    };
+app.use(express.static(__dirname + '/public'));
 
-    res.send(salida);
-
-});
+// app.get('/', (req, res) => {
+//     // res.send('Although')
+//     let salida = {
+//         nombre: 'fernando',
+//         edad:32,
+//         url: req.url
+//     };
+//
+//     res.send(salida);
+//
+// });
 
 app.listen(5000, () => {
     console.log('Escuchandopeticiones en el puerto 3000')
