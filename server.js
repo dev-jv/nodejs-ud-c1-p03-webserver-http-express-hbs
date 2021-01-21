0d8b2f23fafb0385974aa79f55b84bc4ffa03b40
dev-jv <dev.javierleon@gmail.com>
2021-01-21 18:37:42 -0500

    six

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // ---------------------------- <> la versión con express
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
 app.use(express.static(__dirname + '/public'));
 
 // Express HBS engine
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
@@ -12,6 +14,12 @@ app.get('/', (req, res) => {
        name: 'Pure evil',
        year: new Date().getFullYear()
     });
+});
+
+app.get('/about', (req, res) => {
+   res.render('about', {
+       year: new Date().getFullYear()
+   });
 });
 
 // app.get('/', (req, res) => {
